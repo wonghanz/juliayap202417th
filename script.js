4e27e5d95cde5acc23589c264cb5d5421643904a
wonghanz <115807670+wonghanz@users.noreply.github.com>
2024-10-12 01:15:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -105,14 +105,14 @@ function displayPoem() {
         currentLine++;
 
         // Display next line after 8 seconds (5 seconds visible, 3 seconds delay)
-        setTimeout(displayPoem, 8000);
+        setTimeout(displayPoem, 4000);
     } else {
         // Loop back to the first line when the poem is complete
         currentLine = 0;
-        setTimeout(displayPoem, 8000);
+        setTimeout(displayPoem, 4000);
     }
 }
 
 // Start displaying the poem after a short initial delay
-setTimeout(displayPoem, 1000);
+setTimeout(displayPoem, 2000);
 
